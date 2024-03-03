135ceceab5e37948886e3ebd749d7981fbdaffb0
Hao Tu <haotuusa@gmail.com>
2024-03-02 23:56:12 -0800

    initial commit start modularize notebook to python script

---
+++ cnn_blocks.py
@@ -0,0 +1,46 @@
+import torch.nn as nn
+
+class SkipConn2d(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        
+        self.conv = nn.Conv2d(in_channels, out_channels,
+                              kernel_size=1, stride=stride, padding=0)
+            
+        self.bn = nn.BatchNorm2d(out_channels)
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        return x
+    
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 
+                             kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.act1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 
+                               kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        # skip connect projection from in_channels to out_channels
+        # depending on the stride and channels to do a projection
+        # or just pass on the input
+        if stride == 1 and in_channels == out_channels:
+            self.shortcut = nn.Identity()
+        else:
+            self.shortcut = SkipConn2d(in_channels, out_channels, stride)
+        
+    def forward(self, x):
+        # reserve this now for the skip connection
+        shortcut = self.shortcut(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.act1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+
+        # not applying activation here for densenet implementation
+        return x + shortcut
+    
---
+++ custom_cifar10.py
@@ -0,0 +1,22 @@
+from torchvision.datasets import CIFAR10
+from PIL import Image
+
+class CustomCIFAR10(CIFAR10):
+    def __init__(self, root, train=True, download=False, transform=None, focused_transform=None, focused_classes=None):
+        super().__init__(root, train=train, download=download, transform=transform)
+        self.focused_transform = focused_transform
+        # self.focused_classes = focused_classes or []
+
+    def __getitem__(self, index):
+        image, label = self.data[index], self.targets[index]
+        image = Image.fromarray(image)
+        
+        # Apply focused transform to specified classes
+        # if label in self.focused_classes and self.focused_transform is not None:
+        if self.focused_transform is not None:
+            image = self.focused_transform(image)
+        
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image, label
