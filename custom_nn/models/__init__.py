a14ffbe37d910b5be8c92d121dc9e77463c254a0
Hao Tu <haotuusa@gmail.com>
2024-03-24 23:25:00 -0700

    submission

--- custom_nn/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .densenet import DenseMax
-from .dense_max_2_block import DenseMaxTwoBlock
-from .res_max_2_block import ResMaxTwoBlock
-from .res_max_3_block import ResMaxThreeBlock
-
-__all__ = ['DenseMax', 'DenseMaxTwoBlock', 'ResMaxTwoBlock', 'ResMaxThreeBlock']
